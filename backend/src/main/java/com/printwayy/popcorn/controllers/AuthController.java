394e25a7426f3c549057ae973cb4aa83b07be652
Viktor Avelino <viktor.avelino@gmail.com>
2021-11-17 18:46:09 -0500

    Refactoring

--- backend/src/main/java/com/printwayy/popcorn/controllers/AuthController.java
+++ backend/src/main/java/com/printwayy/popcorn/controllers/AuthController.java
@@ -1,10 +1,8 @@
 package com.printwayy.popcorn.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.printwayy.popcorn.models.auth.AuthenticationRequest;
 import com.printwayy.popcorn.models.auth.AuthenticationResponse;
